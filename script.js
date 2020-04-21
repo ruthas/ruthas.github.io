48826a60d0fa33dc43e717688d6bec46a443e6f6
Ruth <ruths@indiefin.com>
2020-04-21 12:34:45 +0200

    added eslint,js

--- script.js
+++ script.js
@@ -7,70 +7,71 @@ const StopwatchAudioDataKey_Stop = "Stopwatch_ResetSound";
 const secondsHand = document.querySelector(".second-hand"); 
 const audio_timerChange = document.querySelector(`audio[data-key=${StopwatchAudioDataKey_Start}]`);
 const audio_timerReset = document.querySelector(`audio[data-key=${StopwatchAudioDataKey_Stop}]`);
-let resetTest = false;
 let timingState = 0; // 0: Ready to run, 1: running, 2: stop
+console.log("hello");
+
 
 function TimerInteraction(){
-    timingState++;
+	timingState++;
 
-    if(timingState > 2){
-        timingState = 0;
-    }
+	if(timingState > 2){
+		timingState = 0;
+	}
 
-    if(timingState == 0){
-        timerReset();
-    }
+	if(timingState == 0){
+		timerReset();
+	}
 
-    if(timingState == 1){
-        timerStart();
-    }
+	if(timingState == 1){
+		timerStart();
+	}
 
-    if(timingState == 2){
-        timerStop();
-}
+	if(timingState == 2){
+		timerStop();
+	}
 }
 
 function timerReset(){
-    timerValue = 0;
-    updateTimer();
-    playAudio(true);
-    document.documentElement.style.setProperty(`--base`, resetBackground);
+	timerValue = 0;
+	updateTimer();
+	playAudio(true);
+	document.documentElement.style.setProperty("--base", resetBackground);
 }
 
 let timerID = 0;
 function timerStart(){
-    playAudio(false);
-    updateTimer();
-    timerID = setInterval(updateTimer, 1000);
-    document.documentElement.style.setProperty(`--base`, startBackground);
+	playAudio(false);
+	updateTimer();
+	timerID = setInterval(updateTimer, 1000);
+	document.documentElement.style.setProperty("--base", startBackground);
 }
 
 function timerStop(){
-    playAudio(false);
-    clearInterval(timerID);
-    document.documentElement.style.setProperty(`--base`, stopBackground);
+	playAudio(false);
+	clearInterval(timerID);
+	document.documentElement.style.setProperty("--base", stopBackground);
 }
 
 
 function updateTimer(){
-    let secondsDegrees = ((timerValue / 60) * 360) + 90;
-    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+	let secondsDegrees = ((timerValue / 60) * 360) + 90;
+	secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    timerValue++;
+	timerValue++;
 }
 
 
 
 function playAudio(isReset){
 
-    let audio = isReset ? audio_timerReset : audio_timerChange;
-    if(!audio) {
-        return;
-    }
-    audio.currentTime = 0;
-    audio.play();
+	let audio = isReset ? audio_timerReset : audio_timerChange;
+	if(!audio) {
+		return;
+	}
+	audio.currentTime = 0;
+	audio.play();
 }
 
-document.documentElement.style.setProperty(`--base`, resetBackground);
+document.documentElement.style.setProperty("--base", resetBackground);
 window.addEventListener("click", TimerInteraction);
 
