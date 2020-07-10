193ecbdf104c3999bfe3964947c3b06af6d1e121
ruthas <62007410+ruthas@users.noreply.github.com>
2020-07-10 09:55:00 +0200

    added . darkmode

--- script.js
+++ script.js
@@ -1,17 +1,60 @@
-const options = {
-	bottom: "64px", // default: '32px'
-	right: "32px", // default: '32px'
-	left: "unset", // default: 'unset'
-	time: "0.3s", // default: '0.3s'
-	mixColor: "#fff", // default: '#fff'
-	backgroundColor: "#fff",  // default: '#fff'
-	buttonColorDark: "#100f2c",  // default: '#100f2c'
-	buttonColorLight: "#fff", // default: '#fff'
-	saveInCookies: false, // default: true,
-	label: "🔆", // default: ''
-	autoMatchOsTheme: true // default: true
-};
-  
-const darkmode = new Darkmode(options);
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
 
-darkmode.showWidget();
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+	"use strict";
+
+	window.addEventListener("load", function () {
+		// Fetch all the forms we want to apply custom Bootstrap validation styles to
+		var forms = document.getElementsByClassName("needs-validation");
+
+		// Loop over them and prevent submission
+		Array.prototype.filter.call(forms, function (form) {
+			form.addEventListener("submit", function (event) {
+				if (form.checkValidity() === false) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
+				form.classList.add("was-validated");
+			}, false);
+		});
+	}, false);
+}());
+
+
+
+//check if dark enabled
+//if enables, turn it off
+//if disable, turn it on
+
+const enableDarkMode = () => {
+	//1. add darkmode class to body
+	document.body.classList.add("darkmode");
+	//2. update darkmode in localstorage
+	localStorage.setItem("darkMode", "enabled");
+	
+};
+
+const disableDarkMode = () => {
+	//1. add darkmode class to body
+	document.body.classList.remove("darkmode");
+	//2. update darkmode in localstorage
+	localStorage.setItem("darkMode", null);
+	
+};
+
+if(darkMode === "enabled"){
+	enableDarkMode();
+}
+
+
+darkModeToggle.addEventListener("click", () => {
+	darkMode = localStorage.getItem("darkMode");
+	if(darkMode != "enabled"){
+		enableDarkMode();
+	}else{
+		disableDarkMode();
+	}
+});
